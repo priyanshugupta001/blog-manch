b66e611ca7f014b0964a0affb8c6f543db05bfd6
Priyanshu <priyanshunita15072002@gmail.com>
2023-07-30 14:48:30 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,47 @@
+import express, { Router } from 'express'
+import cors from 'cors'
+import { connectDb } from './db/database.js'
+import { User } from './model/userModel.js'
+import bcrypt from 'bcrypt'
+import bodyParser from 'body-parser'
+import  jwt  from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
+import userRouter from './routes/Route.js'
+import dotenv from 'dotenv'
+const app = express()
+
+dotenv.config()
+
+// middlerwares
+app.use(express.json())
+app.use(cors({credentials:true,origin:['https://writevix.vercel.app', 'http://localhost:3000']}))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use('/', userRouter)
+
+
+connectDb()
+
+
+app.get("/", (req, res) => {
+	res.json({
+		success: true,
+		message: "worked",
+	})
+})
+
+
+
+
+app.get("/profile", (req,res)=>{
+	const {token} = req.cookies
+	jwt.verify(token, process.env.JWT_SECRET_KEY, {}, (err,data)=>{
+		if(err) throw err;
+		res.json(data)
+	})
+	// res.json(req.cookies)
+})
+
+app.listen(5000, () => {
+	console.log("Connected to server");
+})
